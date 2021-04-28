fe9267b58ebfdcfc1d17565c79bbcfeb909e4548
souro12345 <52583823+souro12345@users.noreply.github.com>
2021-04-28 22:24:07 +0530

    CodeBlock , Markdown , VidioPlayer View Component added

--- src/Components/CodeBlock/CodeBlockView.js
+++ src/Components/CodeBlock/CodeBlockView.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useContext, useState } from "react";
+import SyntaxHighlighter from "react-syntax-highlighter";
+import AddComponent from "../AddComponent/AddComponent";
+import { subtopicContext } from "../Root/Root";
+import { Input, Row, Select, Col, Popover } from "antd";
+import { nightOwl } from "react-syntax-highlighter/dist/esm/styles/hljs";
+import { dark } from "react-syntax-highlighter/dist/esm/styles/prism";
+import classes from "./CodeBlock.module.css";
 
-export default function CodeBlockView() {
+
+export default function CodeBlockView({ component, index }) {
     return (
         <div>
-            CodeBlockView
+             
+            <div className={classes.CodeBlockView} >
+                <SyntaxHighlighter
+                    language={component.language}
+                    style={nightOwl}
+                    showLineNumbers>
+                    {component.content}
+                </SyntaxHighlighter>
+            </div>
         </div>
     )
 }
