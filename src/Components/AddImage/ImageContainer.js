d0d1605d09629830b1d8cdf0f58e8d9ffbdcb392
Ritik Gupta <ritikgupta1982@gmail.com>
2021-05-07 20:33:12 +0530

    video Work done, Image View Halfway

--- src/Components/AddImage/ImageContainer.js
+++ src/Components/AddImage/ImageContainer.js
@@ -1,9 +1,51 @@
-import React from 'react'
+import React, { useState } from "react";
+import classes from "./ImageContainer.module.css";
 
-export default function ImageContainer() {
-    return (
-        <div>
-            ImageContainer
+export default function ImageContainer({ component, index }) {
+  const [showModal, setShowModal] = useState(false);
+
+  return (
+    <div style={{ margin: "20px 0px" }}>
+      {component.isBackground ? (
+        <div
+          className={
+            component.isBackground ? classes.backgroundImageContainer : ""
+          }>
+          <img
+            className={classes.backgroundImage}
+            src={component.src}
+            alt={component.caption}
+            onClick={() => setShowModal(true)}
+          />
+          <p className={classes.imageCaption}>{component.caption}</p>
         </div>
-    )
+      ) : (
+        <div style={{ border: "1px solid #9e99f5", borderRadius: "5px" }}>
+          <img
+            className={classes.normalImage}
+            src={component.src}
+            alt={component.caption}
+            onClick={() => setShowModal(true)}
+          />
+          <p className={classes.imageCaption}>{component.caption}</p>
+        </div>
+      )}
+
+      {showModal && (
+        <div className={classes.modal}>
+          <span className={classes.close} onClick={() => setShowModal(false)}>
+            &times;
+          </span>
+
+          <img
+            className={classes.modalContent}
+            src={component.src}
+            alt={component.caption}
+          />
+
+          <div className={classes.caption}>{component.caption}</div>
+        </div>
+      )}
+    </div>
+  );
 }
--- src/Components/VideoPlayer/VideoPlayer.js
+++ src/Components/VideoPlayer/VideoPlayer.js
@@ -1,32 +1,51 @@
-import { ArrowDownOutlined, ArrowUpOutlined, CloseCircleOutlined, MoreOutlined } from "@ant-design/icons";
+import {
+  ArrowDownOutlined,
+  ArrowUpOutlined,
+  CloseCircleOutlined,
+  MoreOutlined,
+} from "@ant-design/icons";
 import { Button, Input, Popover } from "antd";
-import React, { useState,useContext } from "react";
+import React, { useState, useContext } from "react";
 import ReactPlayer from "react-player/lazy";
 import { subtopicContext } from "../Root/Root";
 import classes from "./VideoPlayer.module.css";
-import IconClasses from '../MoreIcons.module.css'
+import IconClasses from "../MoreIcons.module.css";
+import TextArea from "antd/lib/input/TextArea";
 
-
-
-const UP = -1
-const DOWN = 1 
-
+const UP = -1;
+const DOWN = 1;
 
 export default function VideoPlayer({ component, index }) {
   const [videoURL, setVideoURL] = useState("");
   const [url, setUrl] = useState("");
   const [showVideoPlayer, setShowVideoPlayer] = useState(false);
-  const {subtopic, changeSubtopic, handleMove, RemoveComponent, addComponent} = useContext(subtopicContext)
-
+  const {
+    subtopic,
+    changeSubtopic,
+    handleMove,
+    RemoveComponent,
+    addComponent,
+  } = useContext(subtopicContext);
 
   const onChangeHandler = (e) => {
     setVideoURL(e.target.value);
   };
 
-
+  const captionChange = (e) => {
+    let content = {
+      src: videoURL,
+      caption: e.target.value,
+    };
+    changeSubtopic(index, content, "video");
+  };
   const loadURL = () => {
     setUrl(videoURL);
     setShowVideoPlayer(true);
+    let content = {
+      caption: "video caption",
+      src: videoURL,
+    };
+    changeSubtopic(index, content, "video");
   };
 
   const uploadVideo = () => {
@@ -34,16 +53,25 @@ export default function VideoPlayer({ component, index }) {
   };
   const content = (
     <div className={IconClasses.moreIcons}>
-        <ArrowUpOutlined onClick={() => handleMove(index, UP)} className={IconClasses.moreIcon} />
-        <CloseCircleOutlined onClick={() => RemoveComponent(index)}  className={IconClasses.moreIcon} />
-        <ArrowDownOutlined onClick={()=>handleMove(index, DOWN)}  className={IconClasses.moreIcon} />
+      <ArrowUpOutlined
+        onClick={() => handleMove(index, UP)}
+        className={IconClasses.moreIcon}
+      />
+      <CloseCircleOutlined
+        onClick={() => RemoveComponent(index)}
+        className={IconClasses.moreIcon}
+      />
+      <ArrowDownOutlined
+        onClick={() => handleMove(index, DOWN)}
+        className={IconClasses.moreIcon}
+      />
     </div>
-);
+  );
 
   return (
     <div className={classes.videoComponent}>
-      <div style={{ display: 'flex', justifyContent: "end" }}>
-        <Popover placement="bottomRight" content={content} trigger="hover">
+      <div style={{ display: "flex", justifyContent: "end" }}>
+        <Popover placement='bottomRight' content={content} trigger='hover'>
           <MoreOutlined className={IconClasses.more} />
         </Popover>
       </div>
@@ -54,12 +82,12 @@ export default function VideoPlayer({ component, index }) {
           placeholder='Paste Video URL here'></Input>
         <Button onClick={loadURL} type='primary'>
           Load
-        </Button>  
+        </Button>
       </div>
       {/* <Button onClick={uploadVideo} type='primary'>
         Upload Video
       </Button> */}
-      <br/>
+      <br />
       <div className={classes.playerWrapper}>
         {showVideoPlayer && (
           <ReactPlayer
@@ -67,10 +95,18 @@ export default function VideoPlayer({ component, index }) {
             url={url}
             controls={true}
             width='90%'
-            style={{borderRadius:"10px", overflow:"hidden"}}
+            style={{ borderRadius: "10px", overflow: "hidden" }}
           />
         )}
       </div>
+      <TextArea
+        autoSize={{ minRows: 1 }}
+        maxLength={200}
+        onChange={captionChange}
+        placeholder='Enter Cation'
+        size='large'
+        style={{ borderRadius: "5px", marginTop: "20px" }}
+      />
     </div>
   );
 }
